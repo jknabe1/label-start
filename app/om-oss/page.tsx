d25ba5a5cc63b59371f1d5e7b380eb3c1b3441cf
jknabe1 <jens.knabe@kkrecords.se>
2024-03-28 20:04:25 +0100

    Refactor employee list layout in om-oss page

--- app/om-oss/page.tsx
+++ app/om-oss/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+const employees = [
+  { name: 'Employee 1', image: 'image1.jpg' },
+  { name: 'Employee 2', image: 'image2.jpg' },
+  { name: 'Employee 3', image: 'image3.jpg' },
+  { name: 'Employee 4', image: 'image4.jpg' },
+  { name: 'Employee 5', image: 'image5.jpg' },
+  { name: 'Employee 6', image: 'image6.jpg' },
+];
+
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {employees.map((employee, index) => (
+        <div key={index} style={{ flex: '0 0 50%', padding: '10px' }}>
+          <img src={employee.image} alt={employee.name} style={{ width: '100%' }} />
+          <p>{employee.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default page
+export default Page;
