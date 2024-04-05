51fa18cc7133d954aa185da8f3503bfac461a31f
jknabe1 <jens.knabe@kkrecords.se>
2024-04-05 16:40:14 +0200

    Refactor contact page layout and update body background color

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,30 +1,30 @@
+"use client"
+
 import React from 'react';
 
 const Page: React.FC = () => {
     return (
         <div>
+            <header className="flex flex-col md:flex-row w-full pt-24 border-b-4 border-black">
+                <div className="flex flex-1 items-center justify-center p-4">
+                    <h1 className="text-4xl md:text-6xl font-bold">KONTAKTA OSS</h1>
+                </div>
+                <div className="w-full md:w-[4px] bg-black" />
+                <div className="flex flex-1 items-center justify-center p-4">
+                    <div className='p-24'>
+                        <h2 className="text-xl md:text-3xl font-semibold mb-4">VI VÄNTAR PÅ DITT MEDDELANDE</h2>
+                        <p className="text-xs md:text-sm">
+                            Om du har några frågor om vår verksamhet eller vill skicka in en demo, tveka inte att kontakta oss. Detta formulär är det bästa sättet att nå oss och alla meddelande kommer att besvaras så snart som möjligt.
+                            Detta formulär kan även användas för att rapportera problem med webbplatsen eller för att ge feedback. Vi är tacksamma för alla kommentarer och förslag.
+                        </p>
+                        <p className="text-xs md:text-sm mt-4">
+                          Även band som vill medverka på våra konserter eller som vill skicka in en demo får även gärna använda detta formulär. Vi lyssnar på allt material som skickas in och alla band som skickar in material kommer att få svar. 
+                        </p>
+                    </div>
+                </div>
+            </header>
             <div>
-            <header className="flex w-full">
-      <div className="flex flex-1 items-center justify-center bg-black text-white">
-        <h1 className="text-6xl font-bold">SUBMIT</h1>
-      </div>
-      <div className="w-[2px] bg-gray-200" />
-      <div className="flex flex-1 items-center justify-center p-8">
-        <div>
-          <h2 className="text-3xl font-semibold mb-4">WELCOME TO OUR OFFICIAL SUBMISSION PROCESS</h2>
-          <p className="text-sm">
-            If you’d like to have your music featured on COLORS LISTEN page, please fill out the form below with the
-            relevant information, links and contact details. All submissions will be considered to be featured on the
-            LISTEN page of our website, so please provide a public link to music that you are happy being shared
-            publicly.
-          </p>
-          <p className="text-sm mt-4">
-            Please note that due to the high amount of submissions we receive we’re only able to respond to submissions
-            of interest, and so there’s no need to submit multiple entries for the same artist.
-          </p>
-        </div>
-      </div>
-    </header>
+                
             </div>
         </div>
     );
