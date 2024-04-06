0b0e6ec9498e1dd4fd8178632ee861255f96b07b
jknabe1 <jens.knabe@kkrecords.se>
2024-04-06 13:02:23 +0200

    Refactor artist page, update header component, and fix import issues

--- app/artists/[slug]/page.tsx
+++ app/artists/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useEffect, useState } from 'react'
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from '@/sanity/client'
@@ -25,25 +27,27 @@ const Page = () => {
   
   return (
     <div>
-    <header className="lg:flex flex-col md:flex-row w-full pt-24 border-b-4 border-black sm:border-b-4 dark:border-white">
-                <div className="flex flex-1 items-center justify-center p-4">
-                  <h1 className="text-4xl md:text-6xl font-bold">{artists[0]?.name}</h1>
-                </div>
-                <div className="border-l-4 border-black dark:border-white" />
-                <div className="flex flex-1 items-center justify-center p-4">
-                    <div className='p-24'>
-                    <h2 className="text-xl md:text-3xl font-semibold mb-4">LÄR DIG MER OM OSS OCH VILKA VI ÄR</h2>
-                    <p className="text-xs md:text-sm">
-                      <p> 
-                        Vi är ett ideellt skivbolag som är en del av Kulturföreningen 019. Vi arbetar med att främja ungdomar, främst inom Örebro län. 
-                        Vi arbetar inom kultursektorn och har en bred verksamhet som sträcker sig från musikproduktion till evenemang och utbildning. Läs mer nedan om varför- och hur vi arbetar med vårt syfte och ändamål.
-                      </p>
-                    <p className="text-xs md:text-sm mt-4">
-                    </p>
-                    </p>
-                    </div>
-                </div>
-            </header>
+      <header className="lg:flex flex-col md:flex-row w-full pt-24 border-b-4 border-black sm:border-b-4 dark:border-white">
+        {artists.map((artist: any, index: number) => (
+          <div key={index} className="flex flex-1 items-center justify-center p-4">
+            <h1 className="text-4xl md:text-6xl font-bold">{artist.name}</h1>
+          </div>
+        ))}
+        <div className="border-l-4 border-black dark:border-white" />
+        <div className="flex flex-1 items-center justify-center p-4">
+          <div className='p-24'>
+            <h2 className="text-xl md:text-3xl font-semibold mb-4">LÄS  MER OM {artists.name} </h2>
+            <p className="text-xs md:text-sm">
+              <p> 
+                Vi är ett ideellt skivbolag som är en del av Kulturföreningen 019. Vi arbetar med att främja ungdomar, främst inom Örebro län. 
+                Vi arbetar inom kultursektorn och har en bred verksamhet som sträcker sig från musikproduktion till evenemang och utbildning. Läs mer nedan om varför- och hur vi arbetar med vårt syfte och ändamål.
+              </p>
+              <p className="text-xs md:text-sm mt-4">
+              </p>
+            </p>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
