e4e479a16ee713eab654aa0afbd7916968ce80da
jknabe1 <101703981+jknabe1@users.noreply.github.com>
2024-04-11 12:39:36 +0200

    Update global CSS styles to import fonts from external source

--- app/om-oss/page.tsx
+++ app/om-oss/page.tsx
@@ -55,8 +55,7 @@ const Page = () => {
                     <div className='lg:p-24'>
                     <h2 className="text-xl md:text-3xl font-semibold mb-4">LÄR DIG MER OM OSS OCH VILKA VI ÄR</h2>
                     <p>
-                    Vi är ett ideellt skivbolag som är en del av Kulturföreningen 019. Vi arbetar med att främja ungdomar, främst inom Örebro län. 
-                    Vi arbetar inom kultursektorn och har en bred verksamhet som sträcker sig från musikproduktion till evenemang och utbildning. Läs mer nedan om varför- och hur vi arbetar med vårt syfte och ändamål.
+                    Vi är ett ideellt skivbolag som är en del av Kulturföreningen 019. Vi arbetar med att främja ungdomar, främst inom Örebro län med deras kulturutveckling. 
                     </p>
                     <br/>
                     <p>
@@ -66,10 +65,11 @@ const Page = () => {
                     </div>
                 </div>
         </header>
+        <main className='mx-8'>
         <div className='lg:py-48 sm:py-12 '>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-48">
     <div className="border-4 border-black dark:border-white m-auto overflow-x-hidden col-span-full sm:col-span-2 md:col-span-3"> {/* Here's the change */}
-        <img 
+        <Image
             className='w-full h-full object-cover'
             height={1920}
             alt="{team.name}"
@@ -79,12 +79,71 @@ const Page = () => {
                 width: '100%',
                 height: "full",
             }}
+            loading='lazy'
         />
     </div>
 </div>
             <h1 className="text-2xl sm:text-3xl md:text-4xl text-center lg:py-8 pt-4">Vad är K&K Records</h1>
             <p className='text-center'>Det är vi som står bakom arbetet och vi som ansvarar för att vårt syfte ska följas.</p>
-
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-48">
+                      <div className="border-4 border-black dark:border-white h-90 w-80 md:w-80 m-auto overflow-x-hidden">
+                        <Image 
+                          height={320}
+                          alt=""
+                          width={1080}
+                          src='https://yt3.googleusercontent.com/_iy1wEEEoVUc1be5OCm2nCZFBWIJIh_NuCeVzEefoNcatrOqHKDKkSvBfDRpjWm1_wBvSuyBOw=s900-c-k-c0x00ffffff-no-rj'
+                          style={{
+                            width: '100%',
+                          }}
+                        />
+                        <div className="bg-white dark:bg-black w-full p-4 border-t-4 border-black dark:border-white">
+                          <p className="text-black dark:text-white text-md font-medium divider border-b-2 border-black dark:border-white">
+                            Det ska vara lätt
+                          </p>
+                          <p>
+                            Vi lättar på byråkratin och ser till att det är enkelt att vara medlem och att engagera sig.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="border-4 border-black dark:border-white h-90 w-80 md:w-80 m-auto overflow-x-hidden">
+                        <Image 
+                          height={320}
+                          alt=""
+                          width={1080}
+                          src='https://yt3.googleusercontent.com/_iy1wEEEoVUc1be5OCm2nCZFBWIJIh_NuCeVzEefoNcatrOqHKDKkSvBfDRpjWm1_wBvSuyBOw=s900-c-k-c0x00ffffff-no-rj'
+                          style={{
+                            width: '100%',
+                          }}
+                        />
+                        <div className="bg-white dark:bg-black w-full p-4 border-t-4 border-black dark:border-white">
+                          <p className="text-black dark:text-white text-md font-medium divider border-b-2 border-black dark:border-white">
+                            Det ska vara lätt
+                          </p>
+                          <p>
+                            Vi lättar på byråkratin och ser till att det är enkelt att vara medlem och att engagera sig.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="border-4 border-black dark:border-white h-90 w-80 md:w-80 m-auto overflow-x-hidden">
+                        <Image 
+                          height={320}
+                          alt=""
+                          width={1080}
+                          src='https://yt3.googleusercontent.com/_iy1wEEEoVUc1be5OCm2nCZFBWIJIh_NuCeVzEefoNcatrOqHKDKkSvBfDRpjWm1_wBvSuyBOw=s900-c-k-c0x00ffffff-no-rj'
+                          style={{
+                            width: '100%',
+                          }}
+                        />
+                        <div className="bg-white dark:bg-black w-full p-4 border-t-4 border-black dark:border-white">
+                          <p className="text-black dark:text-white text-md font-medium divider border-b-2 border-black dark:border-white">
+                            Det ska vara lätt
+                          </p>
+                          <p>
+                            Vi lättar på byråkratin och ser till att det är enkelt att vara medlem och att engagera sig.
+                          </p>
+                        </div>
+                      </div>
+                </div>
             </div>
             <div>
             <h1 className="text-2xl sm:text-3xl md:text-4xl text-center lg:py-8 pt-4">Styrelsen</h1>
@@ -138,7 +197,32 @@ const Page = () => {
             <div className='flex flex-col items-center justify-center min-h-screen'>
               <h1 className="text-2xl sm:text-3xl md:text-4xl text-center lg:py-8 pt-4">Bli medlem och få AAA-brickan</h1>
             </div>
-            
+            <div className='lg:py-48 sm:py-12 '>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl text-center lg:py-8 pt-4">Djupgående och mer specifik information</h1>
+            <p className='text-center'>Nedan finns länkar om mer djupgående- och specifik information. Till exempel, hur vi hanterar data, osv.</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-48">
+                      <div className="border-4 border-black dark:border-white h-90 w-80 md:w-80 m-auto overflow-x-hidden">
+                        <Image 
+                          height={320}
+                          alt=""
+                          width={1080}
+                          src='/assets/B05I6324.png'
+                          style={{
+                            width: '100%',
+                          }}
+                        />
+                        <div className="bg-white dark:bg-black w-full p-4 border-t-4 border-black dark:border-white">
+                          <p className="text-black dark:text-white text-md font-medium divider border-b-2 border-black dark:border-white">
+                            Det ska vara lätt.
+                          </p>
+                          <p>
+                            Vi 
+                          </p>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            </main>
     </div>
   )
 }
